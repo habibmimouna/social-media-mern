0febb70579029861ff2feadd9d10076ea92a10f0
habibmimouna <habibmimouna00@gmril.com>
2022-11-22 23:43:53 +0100

    first commit

---
+++ server/routes/posts.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {getposts, createPost} from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getposts);
+router.post('/', createPost);
+export default router;
